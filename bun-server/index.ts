1776582b3c2e020430bd3fedbb3fdf0278ffb400
Julia Eliza <juliaelizaps@gmail.com>
2025-09-17 05:03:39 -0300

    feat: add bun --watch and setup initial server

--- bun-server/index.ts
+++ bun-server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type {Request, Response } from "express"
+
+const app = express();
+const port = 8080;
+
+app.get("/", (response:Response, request:Request)=>{
+    response.json({message:"testing"});
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
